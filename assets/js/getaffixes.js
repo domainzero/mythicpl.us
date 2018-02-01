12d3459cd824ff7b035c5a3bc58458424762fc59
cp3-user <none@none.org>
2018-02-01 14:53:44 +0100

    Changes to multi-language and highlight current week affix
    Highlight current week to affix table in EU-US and more translations to pt and es

--- assets/js/getaffixes.js
+++ assets/js/getaffixes.js
@@ -1,7 +1,31 @@
-getAffixes = function(region) {
+var currentAffixesUS = "";
+var currentAffixesEU = "";
+
+function highlightCurrentAffixes(currentAffixesUS, currentAffixesEU) {
+	
+	// if regions are different, change both
+	if ((currentAffixesUS != currentAffixesEU) && (currentAffixesUS !="" && currentAffixesEU !="")) {
+			document.getElementById(currentAffixesUS).classList.add("table__row-us");
+			document.getElementById(currentAffixesUS).classList.remove("table__row");
+			
+			document.getElementById(currentAffixesEU).classList.add("table__row-eu");
+			document.getElementById(currentAffixesEU).classList.remove("table__row");
+	// if are the same affixes to the regions, highlight just one
+	} else if (currentAffixesUS == currentAffixesEU){
+		document.getElementById(currentAffixesUS).classList.add("table__row-both");
+		document.getElementById(currentAffixesUS).classList.remove("table__row");	
+	};
+	
+	// if any of them is blank, don't do highlight
+	
+}
+
+function getAffixes(region) {
 
     var xhr = new XMLHttpRequest();
 	var affixName = "";
+	var currentAffixes = "";
+		
     xhr.onreadystatechange = function() {
 
         if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 0) {
@@ -36,18 +60,27 @@ getAffixes = function(region) {
                   affix.difficulty = list_affix.difficulty; //idk if this assignment will work, can also put it in a new var
                 }
               });
-
+			  
+			  //get current week affixes key
+			  currentAffixes = affix.name.toLowerCase().substr(0, 2) + currentAffixes;
+			  
 			  //translate the affix name
 			  affixName = translator.get(affix.name);
 			  			  
               //print it
               document.getElementById("thisweek"+region).innerHTML += "<span class='" + affix.difficulty + "'>" + affixName + "</span>" + " ";
             });
-        } 
+
+			if (region == "us") currentAffixesUS = currentAffixes;
+			if (region == "eu") currentAffixesEU = currentAffixes;			
+						
+			highlightCurrentAffixes(currentAffixesUS, currentAffixesEU);
+			
+        };	
     };
     xhr.open('GET', 'https://raider.io/api/v1/mythic-plus/affixes?region='+region, true);
-    xhr.send();
-}
+    xhr.send();	
+};
 
 getAffixes('us');
 getAffixes('eu');
