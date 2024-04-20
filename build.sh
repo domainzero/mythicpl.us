35552b6f90061d22797db29ad3d45f556774c076
domainzero <admin@mythicpl.us>
2024-04-20 13:55:41 -0500

    Initial pass for Season 4

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ mkdir public
 # Grab latest affix information
 curl -fk 'https://raider.io/api/v1/mythic-plus/affixes?region=eu' -o public/affix-eu
 curl -fk 'https://raider.io/api/v1/mythic-plus/affixes?region=us' -o public/affix-us
-curl -fk "https://raider.io/api/v1/mythic-plus/season-cutoffs?season=season-df-3&region=us"  | jq .cutoffs.p999.all.quantileMinValue > public/cutoff-us
-curl -fk "https://raider.io/api/v1/mythic-plus/season-cutoffs?season=season-df-3&region=eu"  | jq .cutoffs.p999.all.quantileMinValue > public/cutoff-eu
+curl -fk "https://raider.io/api/v1/mythic-plus/season-cutoffs?season=season-df-4&region=us"  | jq .cutoffs.p999.all.quantileMinValue > public/cutoff-us
+curl -fk "https://raider.io/api/v1/mythic-plus/season-cutoffs?season=season-df-4&region=eu"  | jq .cutoffs.p999.all.quantileMinValue > public/cutoff-eu
 
 # Copy assets and other files into public
 cp -r assets ads/ads.txt privacy/privacy.html error.html favicon.ico patrons.html news/ public/
