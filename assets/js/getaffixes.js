781aa34eb268e954f0d9ff93fdbec8a47217ec32
domain <matt@hailsatan.xyz>
2025-03-04 11:47:18 -0600

    fix: i felt motivated to fix getaffixes.js

--- assets/js/getaffixes.js
+++ assets/js/getaffixes.js
@@ -1,58 +1,10 @@
 var currentAffixesUS = "";
 var currentAffixesEU = "";
 
-function highlightCurrentAffixes(currentAffixesUS, currentAffixesEU) {
-
-     // if regions are different, change both
-     if ((currentAffixesUS != currentAffixesEU) && (currentAffixesUS != "" && currentAffixesEU != "")) {
-         document.getElementById(currentAffixesUS).classList.add("table__row-us");
-         document.getElementById(currentAffixesUS).classList.remove("table__row");
-
-         document.getElementById(currentAffixesEU).classList.add("table__row-eu");
-         document.getElementById(currentAffixesEU).classList.remove("table__row");
-         // if are the same affixes to the regions, highlight just one
-     } else if (currentAffixesUS == currentAffixesEU) {
-         document.getElementById(currentAffixesUS).classList.add("table__row-both");
-         document.getElementById(currentAffixesUS).classList.remove("table__row");
-     };
-
-     // if any of them is blank, don't do highlight
-};
-
-function fillNextWeeksAffixes(currentAffixesEU) {
-   // As the servers reset from EU are later than the US, it takes the EU as a reference.
-
-   if (currentAffixesEU != "") {
-
-       var row = document.getElementById(currentAffixesEU)
-       var idx = row.rowIndex;
-
-       if (idx == 9) {
-           var nextweek = 10;
-           var weekafternext = 1;
-       } else if (idx > 9) {
-           var nextweek = 1;
-           var weekafternext = 2;
-       } else {
-           var nextweek = idx + 1;
-           var weekafternext = idx + 2;
-       };
-
-       var schedtbl = document.getElementById("sched");
-
-       var nw1 = schedtbl.rows[nextweek].cells[0].innerHTML;
-       var nw2 = schedtbl.rows[nextweek].cells[1].innerHTML;
-       var nw3 = schedtbl.rows[nextweek].cells[2].innerHTML;
-
-       var wan1 = schedtbl.rows[weekafternext].cells[0].innerHTML;
-       var wan2 = schedtbl.rows[weekafternext].cells[1].innerHTML;
-       var wan3 = schedtbl.rows[weekafternext].cells[2].innerHTML;
-
-       document.getElementById("nextweek").innerHTML = "" + nw1 + ", " + nw2 + ", " + nw3;
-       document.getElementById("weekafternext").innerHTML = "" + wan1 + ", " + wan2 + ", " + wan3;
-
-   };
-};
+function trimFirstTwoWords(input) {
+    const words = input.split(' ');
+    return words.length > 2 ? words.slice(2).join(' ') : words[0] || '';
+}
 
 function getAffixes(region) {
     return new Promise(function(resolve, reject){
@@ -67,23 +19,19 @@ function getAffixes(region) {
 
                 var affix_list = [
                     {
-                        "name": "Challenger's Peril",
+                        "name": "Devour",
                         "difficulty": "med",
                     },
                     {
-                        "name": "Xal'atath's Bargain: Devour",
+                        "name": "Voidbound",
                         "difficulty": "med",
                     },
                     {
-                        "name": "Xal'atath's Bargain: Voidbound",
+                        "name": "Ascendant",
                         "difficulty": "med",
                     },
                     {
-                        "name": "Xal'atath's Bargain: Ascendant",
-                        "difficulty": "med",
-                    },
-                    {
-                        "name": "Xal'atath's Bargain: Oblivion",
+                        "name": "Oblivion",
                         "difficulty": "med",
                     },
                     {
@@ -108,25 +56,25 @@ function getAffixes(region) {
 
                     //get the difficulty
                     affix_list.forEach(function(list_affix) {
-                        if (affix.name == list_affix.name) {
+                        if (trimFirstTwoWords(affix.name) == list_affix.name) {
                             affix.difficulty = list_affix.difficulty;
                             affix.link = list_affix.link; //id
                         }
                     });
 
                     //get current week affixes key: 2 first chars and lowercase
-                    currentAffixes = affix.name.toLowerCase().substr(0, 2) + currentAffixes;
+                    currentAffixes = trimFirstTwoWords(affix.name).toLowerCase().substr(0, 2) + currentAffixes;
                     //print it
                     if (affix.link != null){
                         document.getElementById("thisweek" + region).innerHTML += "<span class='" + affix.difficulty + " trn'>" + "<a href='" + affix.link + "'>" + affix.name + "</a></span>" + " ";
                     } else {
-                        document.getElementById("thisweek" + region).innerHTML += "<span class='" + affix.difficulty + " trn'>" + affix.name + "</span>" + " ";
+                        document.getElementById("thisweek" + region).innerHTML += "<span class='" + affix.difficulty + " trn'>" + trimFirstTwoWords(affix.name) + "</span>" + " ";
                     }
                 });
 
                 if (region == "us") currentAffixesUS = currentAffixes;
                 if (region == "eu") currentAffixesEU = currentAffixes;
-
+                console.log(currentAffixesUS)
                 // if the affixes are the same for both regions, don't show both regions
                 if ((currentAffixesUS == currentAffixesEU) && (currentAffixesUS != "" && currentAffixesEU != "")) {
                     document.getElementById("thisweekeu").remove();
@@ -147,14 +95,67 @@ function getAffixes(region) {
         xhr.send();
     });
 
+    function highlightCurrentAffixes(currentAffixesUS, currentAffixesEU) {
+
+         // if regions are different, change both
+         if ((currentAffixesUS != currentAffixesEU) && (currentAffixesUS != "" && currentAffixesEU != "")) {
+             document.getElementById(currentAffixesUS).classList.add("table__row-us");
+             document.getElementById(currentAffixesUS).classList.remove("table__row");
+
+             document.getElementById(currentAffixesEU).classList.add("table__row-eu");
+             document.getElementById(currentAffixesEU).classList.remove("table__row");
+             // if are the same affixes to the regions, highlight just one
+         } else if (currentAffixesUS == currentAffixesEU) {
+             document.getElementById(currentAffixesUS).classList.add("table__row-both");
+             document.getElementById(currentAffixesUS).classList.remove("table__row");
+         };
+
+         // if any of them is blank, don't do highlight
+    };
+
+    function fillNextWeeksAffixes(currentAffixesEU) {
+       // As the servers reset from EU are later than the US, it takes the EU as a reference.
+
+       if (currentAffixesEU != "") {
+
+           var row = document.getElementById(currentAffixesEU)
+           var idx = row.rowIndex;
+
+           if (idx == 7) {
+               var nextweek = 8;
+               var weekafternext = 1;
+           } else if (idx > 7) {
+               var nextweek = 1;
+               var weekafternext = 2;
+           } else {
+               var nextweek = idx + 1;
+               var weekafternext = idx + 2;
+           };
+
+           var schedtbl = document.getElementById("sched");
+
+           var nw1 = schedtbl.rows[nextweek].cells[0].innerHTML;
+           var nw2 = schedtbl.rows[nextweek].cells[1].innerHTML;
+
+           var wan1 = schedtbl.rows[weekafternext].cells[0].innerHTML;
+           var wan2 = schedtbl.rows[weekafternext].cells[1].innerHTML;
+
+           document.getElementById("nextweek").innerHTML = "" + trimFirstTwoWords(nw1) + ", " + nw2;
+           document.getElementById("weekafternext").innerHTML = "" + trimFirstTwoWords(wan1) + ", " + wan2;
+
+       };
+    };
+
 };
+
+
 function highlightCurrentAffixDescriptions(affixes, region) {
         affixes.forEach(function(affix) {
             var name = affix.name;
             var elms = document.querySelectorAll('ul.affixes-list li');
 
             elms.forEach(function(el) {
-                if (classie.has(el, 'affix-' + name.toLowerCase()))
+                if (classie.has(el, 'affix-' + trimFirstTwoWords(name).toLowerCase()))
                 {
                     var ribbon;
                     var ribbonColor = (region === 'us') ? 'red' : 'blue';
@@ -170,7 +171,6 @@ function highlightCurrentAffixDescriptions(affixes, region) {
                         classie.addClass(ribbon, 'green');
                     } else {
                         classie.addClass(el, 'thisweek');
-
                         // Let's add a ribbon
                         ribbon = document.createElement('div');
                         classie.addClass(ribbon, 'ribbon');
