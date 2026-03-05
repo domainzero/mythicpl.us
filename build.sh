338d7b39a1882c3ba569b26d5e7b745736b8b10b
domain <matt@hailsatan.xyz>
2026-03-04 20:00:19 -0600

    Typo fixes and other updates
    how did it never occur to me that this site entirely breaks if you can't load the javascript dude wtf

--- build.sh
+++ build.sh
@@ -13,3 +13,10 @@ cp -r assets ads/ads.txt privacy/privacy.html error.html favicon.ico patrons.htm
 # Concatenate all sections into main page
 cd sections/en/
 cat $(ls | sort -n) > ../../public/index.html
+cd ../..
+
+# Pre-populate affix names for no-JS fallback
+US_AFFIXES=$(jq -r '.affix_details[:2] | map(.name | split(" ") | if length > 2 then .[2:] | join(" ") else .[0] end) | join(" / ")' public/affix-us)
+EU_AFFIXES=$(jq -r '.affix_details[:2] | map(.name | split(" ") | if length > 2 then .[2:] | join(" ") else .[0] end) | join(" / ")' public/affix-eu)
+sed -i "s|__US_AFFIXES__|${US_AFFIXES}|" public/index.html
+sed -i "s|__EU_AFFIXES__|${EU_AFFIXES}|" public/index.html
